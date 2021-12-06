c1d7fe98c6f6b86ea69d21254d71261a65509892
iberdinsky-skilld <iberdinsky@mail.skilld.fr>
2021-12-06 15:59:41 +0600

    Added exernal scripts included

--- packages/components/atoms/title/title.stories.js
+++ packages/components/atoms/title/title.stories.js
@@ -1,22 +1,24 @@
 import DrupalAttribute from "drupal-attribute";
 import { loadFront } from "yaml-front-matter";
 import Twig from "twig";
-import file from "raw-loader!./title.frontmatter.html.twig";
+import file from "!!raw-loader!./title.frontmatter.html.twig";
+import js from "!!raw-loader!./title.js";
 const data = loadFront(file);
 const template = Twig.twig({ data: data.__content });
+import { useEffect, useState } from "@storybook/client-api";
 
 export default {
   title: `Atoms/Title`,
   argTypes: {
-    content: { control: 'text' },
+    content: { control: "text" },
     modifier: {
       options: data.variables.modifiers,
-      control: { type: 'radio' }
+      control: { type: "radio" },
     },
     tag: {
       options: data.variables.template_settings.tag.modifiers,
-      control: { type: 'radio' }
-    }
+      control: { type: "radio" },
+    },
   },
 };
 
@@ -29,9 +31,11 @@ Object.keys(data.variables.attributes).forEach((attribute) => {
   }
 });
 
-export const basic = (args) =>
-  template.render({
-    attributes: attributes.removeClass(data.variables.modifiers).addClass(args.modifier),
+export const basic = (args) => {
+  return template.render({
+    attributes: attributes
+      .removeClass(data.variables.modifiers)
+      .addClass(args.modifier),
     content: {
       template_settings: {
         tag: args.tag,
@@ -39,9 +43,12 @@ export const basic = (args) =>
       content: args.content,
     },
   });
+};
 
-  basic.args = {
-    content: data.default_content.content.content,
-    modifier: data.variables.modifiers[0],
-    tag: data.variables.template_settings.tag.modifiers[0],
-  };
+basic.args = {
+  data,
+  js,
+  content: data.default_content.content.content,
+  modifier: data.variables.modifiers[0],
+  tag: data.variables.template_settings.tag.modifiers[0],
+};
